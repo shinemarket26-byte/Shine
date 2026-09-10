81eb227d88164a455269a74ac1b5434875056634
shinemarket26-byte <shinemarket26@gmail.com>
2026-09-11 00:46:23 +0700

    Create admin-login.js

---
+++ api/admin-login.js
@@ -0,0 +1,24 @@
+export default function handler(req, res) {
+
+  if (req.method !== 'POST') {
+    return res.status(405).json({
+      success: false
+    });
+  }
+
+  const { password } = req.body;
+
+  if (password === process.env.ADMIN_PASSWORD) {
+
+    return res.status(200).json({
+      success: true
+    });
+
+  }
+
+  return res.status(401).json({
+    success: false,
+    message: 'Password salah'
+  });
+
+}
